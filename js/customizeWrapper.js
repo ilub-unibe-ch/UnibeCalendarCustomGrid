7b746acdf2e2ed3788e7755436d281d7b46893f3
Timon Amstutz <timon.amstutz@bluewin.ch>
2019-01-24 22:27:37 +0100

    Improved Modal Display

--- js/customizeWrapper.js
+++ js/customizeWrapper.js
@@ -3,9 +3,32 @@ il.Unibe = il.Unibe || {};
 
 il.Unibe.customizeWrapper = function(uploadId){
     setTimeout(function(){
-            il.UI.uploader.onAllUploadCompleted($(uploadId).attr('id'), function(){
-                location.reload();
+        $(".il-modal-roundtrip").css('z-index', '2000');
+        $(".modal-backdrop").css('z-index', '1900');
+
+        //Copied From src/UI/templates/js/Dropzone/File/uploader.js
+        var $uploadFileLists = $('.il-upload-file-list');
+        $uploadFileLists.find('span.toggle .glyph:first').hide();
+        $uploadFileLists.find('span.toggle .glyph:visible').on('click', function () {
+            console.log("clicke");
+            $(this).parent().find(".glyph").each(function () {
+                $(this).toggle();
             });
+            $(this).parents('.il-upload-file-item').find('.metadata').toggle();
+            return false;
+        });
+        $uploadFileLists.on('click', 'span.remove button.close', function () {
+            var dz = $(this).closest('.il-dropzone-base').find(".il-dropzone");
+            var uploadId = dz.data('upload-id');
+            var fileId = parseInt($(this).parents('.il-upload-file-item').data('file-id'));
+            il.UI.uploader.removeFile(uploadId, fileId);
+            return false;
+        });
+        //End Copy
+
+        il.UI.uploader.onAllUploadCompleted($(uploadId).attr('id'), function(){
+            location.reload();
+        });
     }, 3000);
 
 }
