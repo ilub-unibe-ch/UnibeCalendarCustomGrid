5aa185f5da6f6ac4652075c15c79cd9c97374fc4
Timon Amstutz <timon.amstutz@bluewin.ch>
2019-06-20 15:38:48 +0200

    Fix of #1377

--- js/customizeWrapper.js
+++ js/customizeWrapper.js
@@ -8,4 +8,19 @@ il.Unibe.customizeWrapper = function(uploadId){
         });
     }, 3000);
 
-}
+};
+
+//See Report: 1377, prevent modals from opening twice and not be closable
+$(document).ready(function () {
+    $('#il_center_col .btn-link').on('click', function (e) {
+        console.log("cllicked");
+        setTimeout(function(){
+            var nr_modals = $(".il-modal-roundtrip.in").length;
+            if($(".il-modal-roundtrip.in").length > 1){
+                $(".il-modal-roundtrip.in").slice(1,nr_modals).remove();
+                $(".modal-backdrop.in").slice(0,nr_modals).remove();
+            }
+        }, 3000);
+
+    });
+});
