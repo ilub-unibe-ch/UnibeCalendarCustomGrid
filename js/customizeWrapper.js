6c77ba3ecdce1918098bfbd58518cf8f0392b2b0
Timon Amstutz <timon.amstutz@bluewin.ch>
2020-07-24 14:10:41 +0200

    Fixed #1763

--- js/customizeWrapper.js
+++ js/customizeWrapper.js
@@ -4,7 +4,7 @@ il.Unibe = il.Unibe || {};
 il.Unibe.customizeWrapper = function(uploadId){
     setTimeout(function(){
         il.UI.uploader.onAllUploadCompleted($(uploadId).attr('id'), function(){
-            lslocation.reload();
+            location.reload();
         });
     }, 3000);
 
