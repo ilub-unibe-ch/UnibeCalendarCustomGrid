0aa82ee135d1527c5a14f355ba0b0f348c51cf31
Timon Amstutz <timon.amstutz@bluewin.ch>
2024-08-14 09:22:47 +0200

    Fix missing index in link generation V

--- classes/class.ilUnibeCalendarCustomGridPlugin.php
+++ classes/class.ilUnibeCalendarCustomGridPlugin.php
@@ -109,7 +109,7 @@ class ilUnibeCalendarCustomGridPlugin extends ilAppointmentCustomGridPlugin
                 $words = explode(" ", $name_only);
                 $acronym = "";
                 foreach ($words as $w) {
-                    if(is_array($w) || is_string($w)) {
+                    if((is_array($w) || is_string($w)) && !empty($w)) {
                         $acronym .= $w[0];
                     }
                 }
